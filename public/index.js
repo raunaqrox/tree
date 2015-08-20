3b1fac17da66c5c1e3433967b54922223ac29acf
sahebjot <sahebjot94@gmail.com>
2015-08-21 03:05:05 +0530

    lines connected

--- public/index.js
+++ public/index.js
@@ -9,22 +9,34 @@ $(document).ready(function(){
   var s = Snap('#svg');
 
 
+
 function drawTopNodes(dom){
   var w = width/dom.length;
   var y = 150;
-  var body = drawCircle(width/2, 50,30);
-  var circles = [];
+  var body = drawCircle(width/2 - 30, 50,30,'blue');
+  var nodes = [];
   dom.forEach(function(node, i){
-    circles.push(drawNode(w, y, i, node));
+    nodes.push(drawNode(w, y, i, node));
   });
+}
+//later send parent rather than x2,y2
+function joinToParent(x1,y1, x2,y2, c){
+  return s.line(x1,y1, x2,y2).attr({stroke:c,strokeWidth:5});
 }
 
 function drawNode(w, y, i, node){
-  return drawCircle(w*i, y, 20);
+  return drawCircleAndLine(w*i, y, 20,'red');
 }
 
-function drawCircle(x, y, r){
-  return s.circle(x+r, y, r);
+function drawCircle(x, y, r, c){
+    return  s.circle(x+r, y, r).attr({fill:c});
+}
+
+function drawCircleAndLine(x, y, r,c){
+  return {
+    'circle': drawCircle(x, y, r, c),
+    'line' : joinToParent(x+r,y, width/2, 50, 'gold')
+  };
 }
 
 function parseHtml(html){
@@ -34,6 +46,7 @@ function parseHtml(html){
 
 
   function getHtml(){
+    $('#svg').empty();
     $.get('/get_website',{'url':$website.val()}, function(data){
       if(data!=='err'){
         parseHtml(data);
@@ -42,6 +55,6 @@ function parseHtml(html){
   }
 
   $go.on('click', getHtml);
-
+  getHtml();
 
 });
