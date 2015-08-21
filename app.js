008786385ceaf97ccb02e5e18d8aafd368927732
sahebjot <sahebjot94@gmail.com>
2015-08-21 12:40:42 +0530

    text added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT   || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
--- public/index.js
+++ public/index.js
@@ -24,18 +24,29 @@ function joinToParent(x1,y1, x2,y2, c, sw){
   return s.line(x1,y1, x2,y2).attr({stroke:c,strokeWidth:sw});
 }
 
+function getNodeName(node){
+  var html = node.outerHTML.split('>')[0].split(' ')[0];
+  return html.substr(1,html.length);
+}
+
 function drawNode(w, y, i, node){
-  return drawCircleAndLine(w*i, y, w/4,'red');
+  return drawCircleAndLineAndText(w*i, y, w/4,'red', node, getNodeName(node));
 }
 
 function drawCircle(x, y, r, c){
     return  s.circle(x+r, y, r).attr({fill:c});
 }
 
-function drawCircleAndLine(x, y, r,c){
+function textAt(x, y, t){
+  return s.text(x, y, t);
+}
+
+function drawCircleAndLineAndText(x, y, r, c, node, t){
   return {
     'circle': drawCircle(x, y, r, c),
-    'line' : joinToParent(x+r,y, width/2, 50, 'gold',r/6)
+    'line': joinToParent(x+r,y, width/2, 50, 'gold',r/6),
+    'text': textAt(x+r/2, y+r/2, t),
+    'node': node
   };
 }
 
