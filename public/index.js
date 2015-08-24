c253179dc9c4061bc261ce01f249a7cfef1fe3e5
sahebjot <sahebjot94@gmail.com>
2015-08-24 06:35:59 +0530

    basic child nodes showing on click

--- public/index.js
+++ public/index.js
@@ -7,17 +7,31 @@ $(document).ready(function(){
   $('svg').width(width);
   $('svg').height(height);
   var s = Snap('#svg');
-
-
-
-function drawTopNodes(dom){
-  var w = width/dom.length;
-  var y = 150;
-  var body = drawCircle(width/2 - 30, 50,30,'blue');
   var nodes = [];
-  dom.forEach(function(node, i){
-    nodes.push(drawNode(w, y, i, node));
-  });
+
+
+  function drawBody(dom){
+      var pNode = {
+        x:width/2-30,
+        y:50,
+        r:30
+      };
+      var body = drawCircle(pNode.x, pNode.y, pNode.r, 'blue');
+      drawTopNodes(dom, pNode);
+  }
+
+function drawTopNodes(dom, pNode){
+  var w = width/dom.length;
+  w = w>100?100:w;
+  var y = pNode.y+100;
+  if(dom){
+     // dom.forEach(function(node, i){
+     //   nodes.push(drawNode(w, y, i, node, pNode));
+     // }); 
+     [].forEach.call(dom, function(node, i){  
+        nodes.push(drawNode(w, y, i, node, pNode));
+     });
+  }
 }
 //later send parent rather than x2,y2
 function joinToParent(x1,y1, x2,y2, c, sw){
@@ -25,12 +39,28 @@ function joinToParent(x1,y1, x2,y2, c, sw){
 }
 
 function getNodeName(node){
-  var html = (node.outerHTML || new XMLSerializer().serializeToString(node)).split('>')[0].split(' ')[0];
-  return html.substr(1,html.length);
+  //var html = (node.outerHTML || new XMLSerializer().serializeToString(node)).split('>')[0].split(' ')[0];
+  //return html.substr(1,html.length);
+  return node.tagName;
 }
 
-function drawNode(w, y, i, node){
-  return drawCircleAndLineAndText(w*i, y, w/4,'red', node, getNodeName(node));
+function getChildNodesAndDraw(elem){
+       console.log(elem);
+   nodes.forEach(function(node){
+    if(node.circle.id === elem.srcElement.snap){
+        var pNode = {
+            'x': node.x,
+            'y': node.y,
+            'r': node.r
+        }
+        var childNodes = node.node.childNodes;
+        drawTopNodes(childNodes, pNode);
+    }
+   });
+}
+
+function drawNode(w, y, i, node, pNode){
+  return drawCircleAndLineAndText(w*i, y, w/4,'red', node, getNodeName(node), pNode);
 }
 
 function drawCircle(x, y, r, c){
@@ -41,18 +71,21 @@ function textAt(x, y, t){
   return s.text(x, y, t);
 }
 
-function drawCircleAndLineAndText(x, y, r, c, node, t){
+function drawCircleAndLineAndText(x, y, r, c, node, t, pNode){
   return {
-    'circle': drawCircle(x, y, r, c),
-    'line': joinToParent(x+r,y, width/2, 50, 'gold',r/6),
-    'text': textAt(x+r/2, y+r/2, t),
+    'x': x,
+    'y': y,
+    'r': r,
+    'circle': drawCircle(x, y, r, c).click(getChildNodesAndDraw),
+    'line': joinToParent(x+r, y, pNode.x + pNode.r, pNode.y, 'gold', r/6),
+    'text': textAt(x, y+r/2, t),
     'node': node
   };
 }
 
 function parseHtml(html){
   var newDom = $.parseHTML(html);
-  drawTopNodes(newDom);
+  drawBody(newDom);
 }
 
 
